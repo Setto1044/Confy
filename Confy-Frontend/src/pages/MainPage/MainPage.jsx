a0d4fdc15ebb9fb2ba0643461b8ca0f7748fa0ae
KMJ <kmjk1117@gmail.com>
2025-01-28 23:06:34 +0900

    ✨ Feat: MainPage 네비바
    Former-commit-id: 312f9939e357f1a8934530f107c7a4c4d68c148e

--- Confy-Frontend/src/pages/MainPage/MainPage.jsx
+++ Confy-Frontend/src/pages/MainPage/MainPage.jsx
@@ -1,6 +1,10 @@
+import React from "react";
+import MainNavbar from "../../widgets/Navbar/MainNavbar";
+
 const MainPage = () => {
   return (
     <div>
+      <MainNavbar />
       <h1>MAIN</h1>
     </div>
   );
---
+++ Confy-Frontend/src/widgets/Navbar/MainNavbar.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:46520e9b340f2004b1ac7e15941d9a93438a8d27f601d9fd5abfcfbdc19f513f
+size 2043
