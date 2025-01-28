1320aae140946340b1477d6e73d8a76b5e1e7226
KMJ <kmjk1117@gmail.com>
2025-01-28 22:28:43 +0900

    ✨ Feat: HomePage.jsx에 로그인 버튼
    로그인 버튼 추가
    
    
    Former-commit-id: fda69efde6ec1d282adbf0521b331e7d98224813

--- Confy-Frontend/src/widgets/Button/Button.jsx
+++ Confy-Frontend/src/widgets/Button/Button.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7fc1387746468fec9a6afddacbb8d5bf6500d7572c6b427e60e208066b843b25
-size 922
+oid sha256:c74dc159c87bf8db5f2a24634aced4c794ab74bdf509d4c9ffc0fb8f402968a0
+size 950
--- Confy-Frontend/src/widgets/Navbar/HomeNavbar.jsx
+++ Confy-Frontend/src/widgets/Navbar/HomeNavbar.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d5e784b5fb9504bd4bb6837b54d545756d1289bd7e5d4be733f4212182eb2d6
-size 1693
+oid sha256:fa9cfda33c76e441eba151d16ab90f9fea385e3dd5bd58fea421003416dabc0a
+size 1941
