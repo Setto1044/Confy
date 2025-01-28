9d9c80f4051cf0b1f3709da2ffc1be62577dbe3c
KMJ <kmjk1117@gmail.com>
2025-01-28 23:24:29 +0900

    ⚡️ Feat : MainNavbar 코드 수정
    경로에 meeting이 있을 때만, 로고가 뜸
    
    
    Former-commit-id: 27ec2afefe73c274104fc473d221724ddfa91889

--- Confy-Frontend/src/widgets/Navbar/MainNavbar.jsx
+++ Confy-Frontend/src/widgets/Navbar/MainNavbar.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46520e9b340f2004b1ac7e15941d9a93438a8d27f601d9fd5abfcfbdc19f513f
-size 2043
+oid sha256:6c2d4b7af4424575eac318996279abc6ea2abba96356d2ab770b956b4b006898
+size 2041
