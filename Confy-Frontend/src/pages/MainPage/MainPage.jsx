9e4e07d2b3c9519e75e17f503bc5e54dabebf912
KMJ <kmjk1117@gmail.com>
2025-01-30 17:40:11 +0900

    ✨ Feat: ConferenceStartModel 추가
    Former-commit-id: 33a066041ddad54ae4914b9efce0b1e71d05436b

--- Confy-Frontend/src/pages/MainPage/MainPage.jsx
+++ Confy-Frontend/src/pages/MainPage/MainPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7e7f258d6b39983a6a8713da80a487f7f8266c358faa3710bebeeaf4649ec0f
-size 523
+oid sha256:69d56223e7a0508d217a5dc62a0de9f6ab62890d5a2e3d47a405398d1c2dfce3
+size 1326
---
+++ Confy-Frontend/src/widgets/Modal/ConferenceStartModal.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f8131606e01cd4a0ab1a33765dfe630e2994b687a39216815e164fac46348098
+size 1322
