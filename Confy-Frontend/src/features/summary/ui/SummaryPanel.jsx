dfa95f199c19991d75edcec7f23b4d9e42a4fe03
KMJ <kmjk1117@gmail.com>
2025-01-30 23:32:49 +0900

    ✨ Feat: ProfileModel 탭 바 생성
    Former-commit-id: 3d92ff1e70e2b6c026146fb88ba0bba69d763251

--- Confy-Frontend/src/features/summary/ui/SummaryPanel.jsx
+++ Confy-Frontend/src/features/summary/ui/SummaryPanel.jsx
@@ -4,8 +4,6 @@ const SummaryPanel = () => {
   return (
     <div className="relative p-2 h-92">
       <h2>요약정리</h2>
-      
-      {/* 버튼을 오른쪽 아래로 이동 */}
       <Button className="absolute bottom-4 right-4">수정하기</Button>
     </div>
   );
