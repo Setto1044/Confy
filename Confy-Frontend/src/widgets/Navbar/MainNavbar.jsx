e3b3fa082796341c41a7a1a51745846a5ac80c7d
KMJ <kmjk1117@gmail.com>
2025-01-31 09:32:54 +0900

    ✨ Feat: ProfileModel과 MainNavbar 연결
    Former-commit-id: dc8487c9dbf344e7ac2565a7cb59ff2bee9af095

--- Confy-Frontend/src/widgets/Navbar/MainNavbar.jsx
+++ Confy-Frontend/src/widgets/Navbar/MainNavbar.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4b9a65a112abc65c50e030d0b34efc8721ba96101a2fec84be8a3591e9e1794
-size 2191
+oid sha256:7f1be067280c2619bef0e47acb23eb9e152ffe3c630d8158414d31434c2356f6
+size 2750
