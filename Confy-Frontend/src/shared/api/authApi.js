1a28d972410b1bf578f411d68d8f74b570d046c4
KMJ <kmjk1117@gmail.com>
2025-02-02 19:00:05 +0900

    ✨ Feat: [FE] SummaryPage/api 추가
    summarypage에서만 사용할 api
    
    
    Former-commit-id: 23f3c71c5e93e9352abf32c4c8fdd75fa9e41fca

--- Confy-Frontend/src/shared/api/authApi.js
+++ Confy-Frontend/src/shared/api/authApi.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a95b5f39f352f8d9320af6a35001098451ae33a81237d07d7319b1b54b1bf1ec
-size 2579
+oid sha256:eae188ca1f1685ba0350c4dea53ba10b22da77ed3a5067e9bf5ad8a56b57f715
+size 2352
