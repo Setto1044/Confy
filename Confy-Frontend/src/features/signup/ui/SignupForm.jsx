7e0237022ab9344066aa58e9cd8bd0b65b8b9323
KMJ <kmjk1117@gmail.com>
2025-02-03 16:14:52 +0900

    ✨ Feat: 로그인, 회원가입 서버 연결 완료
    Former-commit-id: 69cb0adfdeb0f56403104fc9bb17ebd38761eaa3

--- Confy-Frontend/src/features/signup/ui/SignupForm.jsx
+++ Confy-Frontend/src/features/signup/ui/SignupForm.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4105d7cf8bbfc9dcb1f168cf50fbd9cd13a83f7c7c40f70fb7b0baf78352c86
-size 4595
+oid sha256:92dfbde567304db333301b306d4caf2031feec9e8815c268dd00572c9ced6f62
+size 4592
--- Confy-Frontend/src/shared/api/authApi.js
+++ Confy-Frontend/src/shared/api/authApi.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d08d534b0f79a4f0a2998a42f0a4b85df19bc01b4a92d2447b2ab6eeabbd4025
-size 1859
+oid sha256:61ba84681886e28401cf2ce07cf332745e78c665a17673315ec955664e26f212
+size 1858
