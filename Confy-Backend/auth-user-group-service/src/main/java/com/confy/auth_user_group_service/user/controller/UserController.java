0243e1303f717a4d25de3db0c6c4466f609fe280
배석진 <bsj1044@naver.com>
2025-02-04 17:59:53 +0900

    ✨ [BE] Auth-User 서버: 회원 이름 수정 기능 추가, application.properties 파일 도커, 로컬 환경 설정 분리
    Former-commit-id: 292e28c6153d1d43a80a555d51de132aa9445fb8

--- Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/user/controller/UserController.java
+++ Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1dac3ab93025f3dae82fe29f6acf1215706ce943c6a6b8260cac2e41d2c37692
-size 4020
+oid sha256:80e3c8a8a99812b050653fac89b7b8bd1cea20f2259276ff90512ccc70c2ee46
+size 4350
