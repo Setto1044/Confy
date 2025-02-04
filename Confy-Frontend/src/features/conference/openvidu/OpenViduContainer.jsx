67e1c19c6a75d569da4584acd4b0ee86620a2225
kmj <kmjk1117@gmail.com>
2025-02-04 21:39:19 +0900

    ✨ Feat: OpenViduContainer tsx->jsx
    Former-commit-id: 4a08a77935db2a52a622a26e0b54cdc5aa1312f4

--- Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
+++ Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9387c636848b653bb3ab4f1225f25b46dd04f0d4a7be227c238005a3e68a9768
-size 7210
+oid sha256:df85ead0a8dc538bc86462d507e6d108ae3e6ccedb517630706caa65b5e8aab6
+size 12831
--- Confy-Frontend/src/shared/store/store.js
+++ Confy-Frontend/src/shared/store/store.js
@@ -7,7 +7,7 @@ const store = configureStore({
     auth: authReducer, // 로그인 관련 상태
     meeting: meetingReducer, // 회의 목록 관련 상태
   },
-  devTools: import.meta.env.MODE !== "production",
+  devTools: import.meta.env.MODE !== "production", // 개발 환경에서만 DevTools 활성화
 });
 
 export default store;
