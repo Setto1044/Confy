e015add2962183996dce84695db023227b3fae62
배석진 <bsj1044@naver.com>
2025-02-04 17:25:16 +0900

    🌐 [BE] 도커파일 생성(Gateway서버, Auth-User-Group 서버), 도커 컴포즈 파일 루트 디렉토리에 추가
    Former-commit-id: 40f2a500996af46a9f14701ab95e7ac07e45d23d

--- Confy-Backend/api-gateway-service/src/main/java/com/confy/api_gateway_service/common/filter/AuthenticationFilter.java
+++ Confy-Backend/api-gateway-service/src/main/java/com/confy/api_gateway_service/common/filter/AuthenticationFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2789d9f3f46b45691b0099fe2a44edc1a511f46ec707d94f01446f53cfa6c98c
-size 3158
+oid sha256:338ab2b707c0271af612d2f5b969c8222624e9176428b7bb7a25a32f4084ee3e
+size 3173
