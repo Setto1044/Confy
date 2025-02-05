d62b68953e2eed743d2f311962bfbf55541edfce
kmj <kmjk1117@gmail.com>
2025-02-05 21:14:17 +0900

    💄 Style: Editvisualization 페이지 css 수정
    달력 토글 완료
    
    
    Former-commit-id: 0fc48dd7a1645fe73c5f6a7284d9e91064590bfc

--- Confy-Frontend/src/pages/EditVisualizationPage/EditVisualizationPage.jsx
+++ Confy-Frontend/src/pages/EditVisualizationPage/EditVisualizationPage.jsx
@@ -1,18 +1,3 @@
-import React from "react";
-import SummaryInfoHeader from "../../features/summary/ui/SummaryInfoHeader";
-import EditVisualization from "../../features/edit-visualization/ui/EditVisualization";
-import SummaryHeader from "../../widgets/Header/SummaryHeader";
-
-const EditVisualizationPage = () => {
-  return (
-    <>
-      <SummaryHeader/>
-      <div>
-      <SummaryInfoHeader />
-        <EditVisualization />
-      </div>
-    </>
-  );
-};
-
-export default EditVisualizationPage;
+version https://git-lfs.github.com/spec/v1
+oid sha256:aaf273293fe5c71bae86527af15b874186d52d2e4e7e57de69c3ecafa44ddec2
+size 1280
