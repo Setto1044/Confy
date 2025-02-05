116b919973075ce7efc092be56a2952183a57163
KMJ <kmjk1117@gmail.com>
2025-02-05 14:40:08 +0900

    💄 Style: summary페이지 시각화, 요약정리 버튼 위치 수정
    정확한 버튼 위치 css는 추후 수정예정
    
    
    Former-commit-id: 7a9697bb37b4b45fd2e5b26039f5cb5c173bdbdf

--- Confy-Frontend/src/pages/SummaryPage/SummaryPage.jsx
+++ Confy-Frontend/src/pages/SummaryPage/SummaryPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44d9b8a2716eafa760e1fa84a9955d57d3e39c12a1be87df6638a7b34c19897b
-size 1781
+oid sha256:29f861c02a08f6fe36157d6914d78269f31309f9187fffe06c89d0ac3c1abbf0
+size 1784
