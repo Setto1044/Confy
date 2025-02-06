258edc401a9c4c7cc0807767c7cf289974756f7b
kmj <kmjk1117@gmail.com>
2025-02-06 21:05:54 +0900

    ✨ Feat: summaryInfoHeader에 데이터 없을 경우 추가
    Former-commit-id: b4a8b8a0803b797b5130fe8003423e05b5d268d4

--- Confy-Frontend/src/features/edit-visualization/hook/useEditVisualizationMutation.js
+++ Confy-Frontend/src/features/edit-visualization/hook/useEditVisualizationMutation.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83a26fdf33b1fa5cc4f2a765b6977cd9ac0698168a7d240f7fef7b64d72f1d64
-size 1006
+oid sha256:6591371190803a66dea93ae628598ba65214f07c62d56468b34e27675ad728ad
+size 1028
