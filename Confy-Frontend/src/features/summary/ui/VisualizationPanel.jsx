1adbf6e48d83c2f675e3619ad01143a653f1407e
kmj <kmjk1117@gmail.com>
2025-02-06 21:05:54 +0900

    ✨ Feat: summaryInfoHeader에 데이터 없을 경우 추가
    Former-commit-id: b4a8b8a0803b797b5130fe8003423e05b5d268d4

--- Confy-Frontend/src/features/summary/ui/VisualizationPanel.jsx
+++ Confy-Frontend/src/features/summary/ui/VisualizationPanel.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4370a591ad0ebf3b39fc7cdd5d68451522affcf3519898c319de7ff69652d9c2
-size 2082
+oid sha256:ce08c275068c10500424fc18f662066a0e95df1525b4ec630f804e7735a64e42
+size 2062
