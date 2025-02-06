768dc22601ff6bacc58e75f78ea81ef4ad704b93
a99yyoung <a99yyoung@naver.com>
2025-02-06 15:30:02 +0900

    ✨ Feat : 회의리스트와 요약페이지 연결
    Former-commit-id: 3de22285dd231bff4a7b6fe1c358143e96fe5870

--- Confy-Frontend/src/widgets/Modal/ConferenceReserveModal.jsx
+++ Confy-Frontend/src/widgets/Modal/ConferenceReserveModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7e901044234d1f295fd0caf572f7ed9c14dfbef143ae7336b96ed9d70632f86
-size 4916
+oid sha256:27cfe5dc1de7d8a103d81a91a0ceb48d4deebd043e0ffed7cc4b1965cb8b4e98
+size 4955
--- Confy-Frontend/src/widgets/Modal/GroupInviteModal.jsx
+++ Confy-Frontend/src/widgets/Modal/GroupInviteModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:738cd2e09921dbbfd777dc6f3b77e5e00e059812ea4958c4f069a4f7485e813c
-size 2141
+oid sha256:062eaa73e5756bd663054aac017271f43daa9dac9b8cf60699f47b989deb2ce1
+size 2559
