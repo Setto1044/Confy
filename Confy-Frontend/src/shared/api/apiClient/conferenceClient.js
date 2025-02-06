7003b41aa9502427e3e74def896ffd5341e120d4
KMJ <kmjk1117@gmail.com>
2025-02-06 13:14:22 +0900

    ✨ Feat: ReactFlow 수정 기능 완료
    1. 수정 후, axios로 데이터 백엔드로 보냄 2. 텍스트 편집기능
    
    
    Former-commit-id: 4777c3ca02e6a4144e7926de6680c99f8257eed9

--- Confy-Frontend/src/shared/api/apiClient/conferenceClient.js
+++ Confy-Frontend/src/shared/api/apiClient/conferenceClient.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb73f67d5fe03d0830d84520ce96bda72da8d9a1bf57a4205a8c5b7a9b25e7df
-size 777
+oid sha256:7878b501d88f7b18338347bff4138b911013870d867036ea83a2d151d821a6f4
+size 805
