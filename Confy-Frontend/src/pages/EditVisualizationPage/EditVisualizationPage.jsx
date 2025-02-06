ff39d063e42d31747fdca1cc142dc71755651226
kmj <kmjk1117@gmail.com>
2025-02-06 22:58:35 +0900

    ♻️ Feat: 시각화 조회 axios json 명칭 변경
    visual -> data로 변경
    
    
    Former-commit-id: 917204b7fb553a0b526a10a1eefbb5fd3a1b8b26

--- Confy-Frontend/src/pages/EditVisualizationPage/EditVisualizationPage.jsx
+++ Confy-Frontend/src/pages/EditVisualizationPage/EditVisualizationPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:106c751feece854a128f927d647e56263caaa844dc4a1b00bbb1ecf3bf94f90b
-size 2415
+oid sha256:4a1408fc85b227cf0e5f840f060bc6c605f8555e80e3cbf3859c096f47eddd3c
+size 2409
