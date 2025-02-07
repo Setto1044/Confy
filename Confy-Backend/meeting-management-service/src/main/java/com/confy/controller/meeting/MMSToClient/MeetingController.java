b3d7ed2956052b3af8809ff6d9b7a28b717341be
노영단 <shdbwjd7052@naver.com>
2025-02-07 13:36:37 +0900

    🚑️ Chore: Update API endpoint URL
    서버 간 API 호출 URL 변경
    
    
    Former-commit-id: 9ab6af65ca86804efd22d2e65cd0fd99cadbf2a2

--- Confy-Backend/meeting-management-service/src/main/java/com/confy/controller/meeting/MMSToClient/MeetingController.java
+++ Confy-Backend/meeting-management-service/src/main/java/com/confy/controller/meeting/MMSToClient/MeetingController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5abe5dfdf9a1f0b76d7f8a9a13d012be5b9613c0e499a5f1191eb15c58925beb
-size 2739
+oid sha256:6407010ec21bb0dc9447bea26d5aa0e631b27a55ea7149963552c65d378a4d01
+size 3017
