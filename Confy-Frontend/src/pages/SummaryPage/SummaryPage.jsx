84c8e0fc57f4a54fd525faf56007d81ee96d8b42
KMJ <kmjk1117@gmail.com>
2025-02-07 21:06:13 +0900

    ♻️ Refact: 시각화 수정 기능 완료
    백엔드와 확인해봐야할듯
    
    
    Former-commit-id: 11c2a1a17e05242b4d0637b9a78c427493e920bc

--- Confy-Frontend/src/pages/SummaryPage/SummaryPage.jsx
+++ Confy-Frontend/src/pages/SummaryPage/SummaryPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26af492d0f08c1557d42ec3ffb04fa923f9df0ea6fa3f4535c92625b43541bbc
-size 3960
+oid sha256:5a4aa13226f28ab2b4e0a68fdba94d7cb92ae366e0bc367b6a10247fefe8ed62
+size 4174
