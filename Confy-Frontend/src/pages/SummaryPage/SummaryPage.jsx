5df6e9a7dca71f19771c83a2e230cb3d675c7786
KMJ <kmjk1117@gmail.com>
2025-02-07 17:29:20 +0900

    ♻️ Refact: 요약페이지 리펙토링 완료
    Former-commit-id: 0a2731bf8863b8a60e3ff48e0c2682aaeb75de3c

--- Confy-Frontend/src/pages/SummaryPage/SummaryPage.jsx
+++ Confy-Frontend/src/pages/SummaryPage/SummaryPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ba2032064874f1437d080d19c2cddc893ff24d1802a04effbd3c411679f7002
-size 3901
+oid sha256:26af492d0f08c1557d42ec3ffb04fa923f9df0ea6fa3f4535c92625b43541bbc
+size 3960
