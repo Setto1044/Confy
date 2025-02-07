d300c0e5ebb97e9ed4b838afe9a7b2e7054c0db7
KMJ <kmjk1117@gmail.com>
2025-02-07 09:02:02 +0900

    ♻️ Refact : editvisualization페이지 폴더 구조 변경
    Former-commit-id: 39ec18ea1f26ee9f4faa39a5004a3fec0d938556

--- Confy-Frontend/src/features/conference/Sidebar/Sidebar.jsx
+++ Confy-Frontend/src/features/conference/Sidebar/Sidebar.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:958338510cd03fee3a17defaef5d46a537c8d57722aba5db942e7250ea8f8966
-size 1283
+oid sha256:a18ec90da4b435630126b891c47ac6d752af26d86180605a526bb46c2b33c024
+size 1284
