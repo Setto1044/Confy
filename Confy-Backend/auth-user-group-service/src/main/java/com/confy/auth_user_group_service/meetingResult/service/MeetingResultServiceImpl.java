ef9682fe22639eeb1e96e287db07b99420334d5a
배석진 <bsj1044@naver.com>
2025-02-07 14:58:11 +0900

    ♻️ Refactoring: WebClient null 응답 반환 예외 처리
    Former-commit-id: be92b35a100abdbbfd97a2371144469d8293e17c

--- Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/meetingResult/service/MeetingResultServiceImpl.java
+++ Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/meetingResult/service/MeetingResultServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:797dff3162b121533ace1736e194e78c648f91df06a354dd6fd131cc7c446f65
-size 1691
+oid sha256:995def401f05753ae4a899fd560d628a79bc758fe3200b0107e8295f35b2d1f2
+size 2594
