71c2257f46ef78f11920aa0205b561f71c574c83
a99yyoung <a99yyoung@naver.com>
2025-02-07 18:09:13 +0900

    ✨ Feat : 그룹 관련 API 연결
    Former-commit-id: b8454e32454654f11656e7a0db0bcc6b78583fd9

--- Confy-Frontend/src/features/main/MeetingList/ui/MeetingItem.jsx
+++ Confy-Frontend/src/features/main/MeetingList/ui/MeetingItem.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:099b1f635aa3ae0b39c38d0dfd8ba08b5bc5ba95355aa7a81e93bd01e5560ba2
-size 1750
+oid sha256:28d1ff4bb7b6cab9cac720a1e8c2115188b2979e07871797d0965aee2b6993cc
+size 2133
--- Confy-Frontend/src/features/main/Sidebar/GroupList.jsx
+++ Confy-Frontend/src/features/main/Sidebar/GroupList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16c31168d1045cdd8a4c17b47fc0272d361a84a415140d71424c23d6352c1b3e
-size 2435
+oid sha256:13d012ace102bdd5bb2befd55f75bb45e303a25f46667258be118d49deb3abd7
+size 4428
