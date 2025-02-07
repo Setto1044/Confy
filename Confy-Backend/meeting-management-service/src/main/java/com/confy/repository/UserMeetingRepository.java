55487935a503a2b9175d20228db7cefe308b66a2
노영단 <shdbwjd7052@naver.com>
2025-02-07 17:49:59 +0900

    ✨ Feat: Add API for leaving a meeting
    회의방 나가기 기능을 제공하는 API 추가
    
    
    Former-commit-id: eb19c48366a0ad9ee33eba8f1f69be8e84fa6551

--- Confy-Backend/meeting-management-service/src/main/java/com/confy/repository/UserMeetingRepository.java
+++ Confy-Backend/meeting-management-service/src/main/java/com/confy/repository/UserMeetingRepository.java
@@ -1,13 +1,3 @@
-package com.confy.repository;
-
-import com.confy.entity.Meeting;
-import com.confy.entity.UserMeeting;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-@Repository
-public interface UserMeetingRepository extends JpaRepository<UserMeeting, Integer> {
-    Optional<UserMeeting> findByMeetingAndUserId(Meeting meeting, Long userId);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:1f811994a365e72eab79680c16c8ce3635952c45baecaedfca843b78c634285f
+size 969
