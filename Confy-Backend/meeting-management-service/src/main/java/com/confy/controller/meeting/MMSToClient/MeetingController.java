143b57fe13c130565d3277bf86ae1c59560cfd48
노영단 <shdbwjd7052@naver.com>
2025-02-07 17:49:59 +0900

    ✨ Feat: Add API for leaving a meeting
    회의방 나가기 기능을 제공하는 API 추가
    
    
    Former-commit-id: eb19c48366a0ad9ee33eba8f1f69be8e84fa6551

--- Confy-Backend/meeting-management-service/src/main/java/com/confy/controller/meeting/MMSToClient/MeetingController.java
+++ Confy-Backend/meeting-management-service/src/main/java/com/confy/controller/meeting/MMSToClient/MeetingController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcb3cb4494c1deab65af8f78c8bf6381b1a0b6be99dae8d7e76c670af7210923
-size 1623
+oid sha256:fe34e9293f2a56f88e11e971bacd72d7da9f2eaa3bcbbded5439107d0ff10999
+size 2234
