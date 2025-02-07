4e732a3049f28ef02fa27fd4ebc17dbcf0b9440e
노영단 <shdbwjd7052@naver.com>
2025-02-08 01:57:45 +0900

    ♻️ Refactor: Update request/response headers and body
    Former-commit-id: 0b029ebef7114c50d4447ac537cfec02236e9a7f

--- Confy-Backend/meeting-management-service/src/main/java/com/confy/controller/openVidu/OpenViduController.java
+++ Confy-Backend/meeting-management-service/src/main/java/com/confy/controller/openVidu/OpenViduController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:faf767d963adcacb4796edb9dba8b27f512aa85a29137cd7f25c242c7125b51b
-size 3516
+oid sha256:9533dbe21d6c5c7b3485194eae667fc557d22f58c95098f89823a6840587b2eb
+size 3521
