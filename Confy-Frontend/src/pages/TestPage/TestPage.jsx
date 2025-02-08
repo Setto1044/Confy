cdf04a60cab246cc75bbdc8e3f278e54bd45ff6d
KMJ <kmjk1117@gmail.com>
2025-02-09 05:18:55 +0900

    💄 Design : 화상회의 화면 CSS 수정
    패널 펼쳐지면 요소들 좁아지게 수정중
    
    
    Former-commit-id: 8e1b1d775617fac2feef304d7f31badde679884c

--- Confy-Frontend/src/pages/TestPage/TestPage.jsx
+++ Confy-Frontend/src/pages/TestPage/TestPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0d2281c9bfd7eafe6ee773ce62e4c7c42be6250a25a029dca73d1c29655d896
-size 859
+oid sha256:1e529c62a0ecd22a4cd0a22af00805fc37269b94db551c798d5ac2a3d980301b
+size 633
