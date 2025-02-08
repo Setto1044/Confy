8979f55b5be6ccaf86908ed28ff51635061a543d
KMJ <kmjk1117@gmail.com>
2025-02-09 01:08:26 +0900

    ✨ Feat : TestPage에서 오픈비두 UI 작업
    Former-commit-id: 1bdbc82f23290c80226aad5d4356768e908627a4

--- Confy-Frontend/src/pages/TestPage/TestPage.jsx
+++ Confy-Frontend/src/pages/TestPage/TestPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import MeetingRoom from "../../features/conference/openvidu/ui/MeetingRoom";
 import Sidebar from "../../features/conference/Sidebar/Sidebar";
 
 const TestPage = () => {
   return (
     <>
         <h1>사이드바 테스트 페이지</h1>
+        <MeetingRoom/>
         <Sidebar />
     </>
   );
