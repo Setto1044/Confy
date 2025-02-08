c8d2d00684578ccb184ce402e7e734e8efe537b7
배석진 <bsj1044@naver.com>
2025-02-08 20:37:13 +0900

    ♻️ vite config ts 파일 괄호 오타 수정
    Former-commit-id: 71244f5f98e5c7bc4ce94430033599ad2ae2f4b6

--- Confy-Frontend/vite.config.ts
+++ Confy-Frontend/vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    tailwindcss()],
 
-  ],
+  server: {
+    host: '0.0.0.0',
+    port: 5080,
+    allowedHosts: ['i12a508.p.ssafy.io']
+  },
+  base: '/'
 });
