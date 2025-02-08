a12efa0724a1af204cc56b69a43ab099ec54fc43
KMJ <kmjk1117@gmail.com>
2025-02-08 19:58:47 +0900

    💄 Design : 패널 토글시, 아이콘 전환 구현
    Former-commit-id: 88431c4fc4ff81ae0cdca247818e87a8f66bb89f

--- Confy-Frontend/src/features/conference/Sidebar/Sidebar.jsx
+++ Confy-Frontend/src/features/conference/Sidebar/Sidebar.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92a3003796cdf0b229b3f6eb22420c07b5b1fbd79af45b902bd07d9de0a87bb6
-size 2597
+oid sha256:bd56a60d0f4694dbc70c26817c3afe4b7e8f5a93a39e1191f850b6086c34fca5
+size 3652
