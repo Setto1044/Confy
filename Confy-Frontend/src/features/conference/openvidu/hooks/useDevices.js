d16d9264c08cdf493b9909598874be3c3e7c085f
KMJ <kmjk1117@gmail.com>
2025-02-09 00:21:40 +0900

    ♻️ Refact : MeetingForm, MeetingRoom 컴포넌트 분리
    Former-commit-id: 290e69045c78390a102c73b09913c372e258b4b6

---
+++ Confy-Frontend/src/features/conference/openvidu/hooks/useDevices.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a9d7ebaea2be52480daf2848ce3221b1e06906185fe7f6db46c3c053e9129e42
+size 1225
--- Confy-Frontend/src/features/conference/openvidu/hooks/useLiveKit.js
+++ Confy-Frontend/src/features/conference/openvidu/hooks/useLiveKit.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5d470e9d421d360ed16efe4cf529c0705d9873bcc4d5e9211a63922476f22e1
-size 3226
+oid sha256:d4b1e5b9351c5911f4e25eb81c742ead1f9cc8207407b4ceed44d4b8dba4fdde
+size 5467
