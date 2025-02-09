b132ae6202e8bc97f7e656265cbb28cb779f6936
KMJ <kmjk1117@gmail.com>
2025-02-09 16:19:43 +0900

    ♻️ Refact : openviduContainer.js 리팩토링 이후 시능 확인
    mock서버 사용해서 기능 문제 없는 지 확임 함
    
    
    Former-commit-id: e58a805099746ec443e47d671f384217346f5cd6

--- Confy-Frontend/src/app/route.jsx
+++ Confy-Frontend/src/app/route.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:248e6c0abcbcdd86a19480852782a7153e123e6c2a37ca7e2d5ec567f1f7265b
-size 1792
+oid sha256:6559c168d7c5fd7f05ba9c25467d68ab5c3360dd4a89bd37b357eeef80fcc3a8
+size 1786
