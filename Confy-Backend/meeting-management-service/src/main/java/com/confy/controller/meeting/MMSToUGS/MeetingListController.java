320e54110f27f04fdb0b76246d6dd95a7f2a7619
노영단 <shdbwjd7052@naver.com>
2025-02-09 22:37:22 +0900

    ✨ Feat: Implement meeting list API with infinite scroll
    무한 스크롤 기능을 위한 회의 리스트 조회 API 구현
    
    
    Former-commit-id: 5ff8c290570a4c06bea24f2c3edd76c9c9859c31

---
+++ Confy-Backend/meeting-management-service/src/main/java/com/confy/controller/meeting/MMSToUGS/MeetingListController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e5463c91cb7d5e5b661958f9ea00c1a4321d20d38c4624ed5222b7946f06f733
+size 3501
---
+++ Confy-Backend/meeting-management-service/src/main/java/com/confy/service/meeting/MMSToUGS/MeetingListServiceImpl.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c4c9eb27dcf38947f5a656ce81fd149fb5f743acc8be73cd909970f0e0a4ae52
+size 2013
