c68a93415e12d0e720dfb587144374b4437aaae5
KMJ <kmjk1117@gmail.com>
2025-02-09 18:16:42 +0900

    💄 Design : 화상회의 페이지 헤더추가
    Former-commit-id: 7166b8973ef335848d511fe612235d758bd9ad70

--- Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
+++ Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f405a049467ee35f565a82de7b2db495ebc9ee2f7c1b2d4fed677956efaf77d
-size 5427
+oid sha256:8ca83dc71bee6de37ed6bc3eddab754fa72f55d3a27c033eaa5ff249702ab05d
+size 5522
