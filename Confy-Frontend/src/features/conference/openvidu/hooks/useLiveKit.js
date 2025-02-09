b3bd51d43f20855043c74ba8f5cdebbae88bdb18
KMJ <kmjk1117@gmail.com>
2025-02-09 16:19:43 +0900

    ♻️ Refact : openviduContainer.js 리팩토링 이후 시능 확인
    mock서버 사용해서 기능 문제 없는 지 확임 함
    
    
    Former-commit-id: e58a805099746ec443e47d671f384217346f5cd6

--- Confy-Frontend/src/features/conference/openvidu/hooks/useLiveKit.js
+++ Confy-Frontend/src/features/conference/openvidu/hooks/useLiveKit.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4b1e5b9351c5911f4e25eb81c742ead1f9cc8207407b4ceed44d4b8dba4fdde
-size 5467
+oid sha256:1225ba65c050d7fdebcb0b9191a0fc712ae2bf6d02a92f4a5feedb118e14ff04
+size 8002
