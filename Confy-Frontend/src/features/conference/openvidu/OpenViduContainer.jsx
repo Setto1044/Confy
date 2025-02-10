ec5f9b58f48b55f2a0bfd0370cdf03c87a9f0346
KMJ <kmjk1117@gmail.com>
2025-02-10 14:17:49 +0900

    ⚡️ Error : setHasCamara() 문제 해결중
    Former-commit-id: 91a7d31ec76c13b6b706f593c53eccd8ee20f82c

--- Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
+++ Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7b5103ffe72ebc6079e242c39363ec287530238e4ffa06288216b5e000d3b86
-size 4805
+oid sha256:75b30e820446d16acac88675b6a415db5b2a6ea981ec11e8b17e95de514e0e4d
+size 4889
