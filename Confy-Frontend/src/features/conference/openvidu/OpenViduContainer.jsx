adfd95b1a1ab2c2cfc12084931f2289d5e2ee432
KMJ <kmjk1117@gmail.com>
2025-02-10 12:47:50 +0900

    🔀 Merge : OpenViduCOntainer.jsx 의 stopSTT 오류 해결
    Former-commit-id: b66dc2abddc474d0516ff9dfc45a0e6e5b93a723

--- Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
+++ Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06d889ee185b8975b917a72d36a2defa07f6deaf38e259e22ef1a62d63ed53c6
-size 5340
+oid sha256:abee752b19267c14fa8fb3d75c03f5e677ee5afd89fbf927f3ec574e8042e61a
+size 4604
--- Confy-Frontend/src/features/conference/openvidu/hooks/useLiveKit.js
+++ Confy-Frontend/src/features/conference/openvidu/hooks/useLiveKit.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afcb54747c3962ed54064911fbdcf74b2b705ae1288d6a3d86ba5490cccb888c
-size 9655
+oid sha256:d886b549945c06dc79f02f5f6f8cae8f95b0c7dc05ff48af34c7dc6b589793e7
+size 10078
