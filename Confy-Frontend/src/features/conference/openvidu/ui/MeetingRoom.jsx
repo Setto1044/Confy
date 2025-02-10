1e523e2cc77958fc70bf6c96d38be5af6cef0339
KMJ <kmjk1117@gmail.com>
2025-02-10 09:46:15 +0900

    ✨ Feat: 오픈비두모듈화
    Former-commit-id: d8d7114a96ca4a7751313e629a55fcce8298ddb9

--- Confy-Frontend/src/features/conference/openvidu/ui/MeetingRoom.jsx
+++ Confy-Frontend/src/features/conference/openvidu/ui/MeetingRoom.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:296eb367f3f8ae92c634432b449b8a142bfee1c50b4c8942fc210993a6ac6471
-size 2620
+oid sha256:b888340f23cbf3ff2aab14e7fb470929e2acef512254558524bad26d6cdb856a
+size 2626
--- Confy-Frontend/src/widgets/Modal/ConferenceCreateModal.jsx
+++ Confy-Frontend/src/widgets/Modal/ConferenceCreateModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfb218e94ddcdf02fcb714728385c916d176e5d60fdd91ab9062230316b473e7
-size 3263
+oid sha256:207f4229b050b8ada49c5d55b071f098fdd2a254bc32efbc44494cb9b0f4bb47
+size 6002
