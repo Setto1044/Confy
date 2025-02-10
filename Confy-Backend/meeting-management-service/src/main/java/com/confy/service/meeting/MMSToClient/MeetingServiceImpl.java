c73eedf71c5d0a8ecf93645481625d1e4d57fa3a
노영단 <shdbwjd7052@naver.com>
2025-02-10 12:36:31 +0900

    🚑️ Fix: Resolve data exchange issue between member group and meeting servers
    회원 그룹 서버와 미팅 서버 간의 데이터 송수신 과정에서 발생하던 버그 수정
    
    
    Former-commit-id: 3e4cd09a91044e3c8d29935840e51929cc320288

--- Confy-Backend/meeting-management-service/src/main/java/com/confy/service/meeting/MMSToClient/MeetingServiceImpl.java
+++ Confy-Backend/meeting-management-service/src/main/java/com/confy/service/meeting/MMSToClient/MeetingServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e9fea09b346f487074d4a9294dcae946f9ff2ee043dc67c54bc87ce4ee58f7a
-size 5611
+oid sha256:6aed82a15515e5ac3a9efba7c4d6049387eae2d95edf9d05098974d4769011c2
+size 5787
