ad3dc5a5791d2422d52e97edf30a54ad35168951
KMJ <kmjk1117@gmail.com>
2025-02-10 12:47:50 +0900

    🔀 Merge : OpenViduCOntainer.jsx 의 stopSTT 오류 해결
    Former-commit-id: b66dc2abddc474d0516ff9dfc45a0e6e5b93a723

--- Confy-Frontend/src/features/conference/openvidu/api/configureUrls.js
+++ Confy-Frontend/src/features/conference/openvidu/api/configureUrls.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:592540cc0759e65cb7c18b499f12edd660414e44ade642a6e26317d1403382ce
-size 2011
+oid sha256:8b43dcd476c31fe56f87de66616b11b8107771fd6c865509ebd9a1f73227a9b3
+size 2023
