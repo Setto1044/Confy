cb1d7f4094742e6077220a65019ecfbec7e8d74e
노영단 <shdbwjd7052@naver.com>
2025-02-11 01:23:49 +0900

    ✨ Feat: Implement API for summarizing STT data using GPT -mRedis에 저장된 실시간 STT 데이터에서 특정 시간대의 스크립트를 조회하여 GPT를 활용한 요약 기능을 구현함
    Former-commit-id: 53cc52c4e783a6c63070a1e4eece16f8518f1a6c

--- Confy-Backend/meeting-management-service/src/main/java/com/confy/controller/meeting/MMSToClient/MeetingController.java
+++ Confy-Backend/meeting-management-service/src/main/java/com/confy/controller/meeting/MMSToClient/MeetingController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:122f7744b4f69269545da2eef2b39dff4d166987fce8c35b8b031d11b8ec2467
-size 5517
+oid sha256:5431be3e0e5f61bf7ab5d71dc200f9c6971f942867ad4eab7ff46ac28c5dfb99
+size 6112
--- Confy-Backend/meeting-management-service/src/main/java/com/confy/service/meeting/MMSToClient/MeetingServiceImpl.java
+++ Confy-Backend/meeting-management-service/src/main/java/com/confy/service/meeting/MMSToClient/MeetingServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51f6a2a9d0a97fa33665c9af74075a631b3d39d08f94cc582eab0db8dcf0636a
-size 6684
+oid sha256:485c0422c39d8705ea465633b56d277817e60ca1f32a0fc08dcf8b65a8860191
+size 6696
