24a87ce006b6857e98ac9286ab1137706b2000ac
yejin <kimyejin0044@gmail.com>
2025-02-11 15:37:43 +0900

    :sparkles: Feat: Implemented saveSummary, saveJson, saveKeyword
    Former-commit-id: 8445f63b84f4569f35d4dd8764ad73689fa4c28a

--- Confy-Backend/meeting-management-service/src/main/java/com/confy/service/meeting/MMSToClient/MeetingResultServiceImpl.java
+++ Confy-Backend/meeting-management-service/src/main/java/com/confy/service/meeting/MMSToClient/MeetingResultServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:baac76239b282739b7826559b608d30a46047617d5a4445a85be8f918d0c5a3c
-size 4351
+oid sha256:c67de2dc845f65800f8a79d5effe5225045dee73df3eda3d171d9dd136dd4312
+size 6416
--- Confy-Backend/meeting-management-service/src/main/java/com/confy/service/meeting/MMSToClient/MeetingServiceImpl.java
+++ Confy-Backend/meeting-management-service/src/main/java/com/confy/service/meeting/MMSToClient/MeetingServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:485c0422c39d8705ea465633b56d277817e60ca1f32a0fc08dcf8b65a8860191
-size 6696
+oid sha256:a5d7a1923ace4fd51f70fd6b7ea13e4804512c192e4921ca222ba509649039ab
+size 6873
