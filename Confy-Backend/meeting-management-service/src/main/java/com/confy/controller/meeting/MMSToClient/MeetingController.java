d46f469f6be0e1cd0290fd91fc6f3d2367c715e8
노영단 <shdbwjd7052@naver.com>
2025-02-11 09:47:51 +0900

    ♻️ Refactor: Modify response wrapping on GPT summary request failure
    GPT 요약 요청 실패 시 `ResponseEntity.ok()` 대신 `ResponseEntity.status(500).body()`로 변경
    
    
    Former-commit-id: 1f6424cbe8e634d528107349736d0a6687b8e485

--- Confy-Backend/meeting-management-service/src/main/java/com/confy/controller/meeting/MMSToClient/MeetingController.java
+++ Confy-Backend/meeting-management-service/src/main/java/com/confy/controller/meeting/MMSToClient/MeetingController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5431be3e0e5f61bf7ab5d71dc200f9c6971f942867ad4eab7ff46ac28c5dfb99
-size 6112
+oid sha256:fa0145f49ece42d4e60050aaa7594b5f2f38a33dd7251f529a2e6bad0704a51d
+size 6126
