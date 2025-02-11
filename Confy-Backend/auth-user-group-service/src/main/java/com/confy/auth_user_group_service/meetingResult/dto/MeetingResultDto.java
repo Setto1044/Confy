533bfafc8064e4feda952e32a96188ff9c8f0b4c
배석진 <bsj1044@naver.com>
2025-02-11 13:21:48 +0900

    🐛 Fix: 회의 그룹 정보 제공 누락 문제 해결
    Former-commit-id: fe2432b2c88550daf682e0395aef1b2ea7a27880

--- Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/meetingResult/dto/MeetingResultDto.java
+++ Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/meetingResult/dto/MeetingResultDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12a6d3b036e78e866d2bf5fc5dc54e37d81691f5d0bd267d03d036df7c8b4ad8
-size 558
+oid sha256:4ef503ce1681ff8743bf5118ead4dc90f91aab1a1b9a62bc0b4035f20279e5c5
+size 566
