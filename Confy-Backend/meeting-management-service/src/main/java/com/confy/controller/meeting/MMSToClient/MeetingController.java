c359f32a99d23b7dfcb5505dd29ff2be051bf9fb
a99yyoung <a99yyoung@naver.com>
2025-02-11 16:03:39 +0900

    ✨ Feat : OpenVidu API 연결
    Former-commit-id: a43e707883e4bb6e0b251492ead5b8601f503e55

--- Confy-Backend/meeting-management-service/src/main/java/com/confy/controller/meeting/MMSToClient/MeetingController.java
+++ Confy-Backend/meeting-management-service/src/main/java/com/confy/controller/meeting/MMSToClient/MeetingController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa0145f49ece42d4e60050aaa7594b5f2f38a33dd7251f529a2e6bad0704a51d
-size 6126
+oid sha256:62e321d9d5911d311c968d7c4981c46994f5d800d230652fbaefd59c74222ade
+size 6298
--- Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
+++ Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28fa6a16d3fd90baae8f2cb58418998bb8741fdc1ab025f2eda8131cfe76e02d
-size 4838
+oid sha256:49768cc18bcb3e06ff058c0f086feff25728233fce5d3182842d66d7afcbb04c
+size 15982
