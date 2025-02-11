4a2808517f89ef52cd4a4d6d00f1e4aee2b395f0
배석진 <bsj1044@naver.com>
2025-02-11 10:43:51 +0900

    🐛 Fix: 인증 필터 CORS 문제 수정
    Former-commit-id: 88219fd2a786ca90f4875fa1bea98ecc2bc9a575

--- Confy-Backend/api-gateway-service/src/main/java/com/confy/api_gateway_service/common/filter/AuthenticationFilter.java
+++ Confy-Backend/api-gateway-service/src/main/java/com/confy/api_gateway_service/common/filter/AuthenticationFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4799cbd426ecf5979f6be9673be30486942845b260b44c17978a5950ea57bd11
-size 6311
+oid sha256:874cf037d8070e300a12ed94e06a2486c76f5a1446f8afc04f35b1f51c3a92da
+size 6379
