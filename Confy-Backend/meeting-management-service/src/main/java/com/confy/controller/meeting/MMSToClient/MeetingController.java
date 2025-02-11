ed29ec4cd59be32a16e26fb2c7fc55a5181e77e3
배석진 <bsj1044@naver.com>
2025-02-11 16:06:44 +0900

    🐛 Fix: CORS 헤더 중복 문제 필터로 해결
    Former-commit-id: 111306ae2c5ebdf98dc5b1f2031a8a5abb4f32a9

--- Confy-Backend/meeting-management-service/src/main/java/com/confy/controller/meeting/MMSToClient/MeetingController.java
+++ Confy-Backend/meeting-management-service/src/main/java/com/confy/controller/meeting/MMSToClient/MeetingController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa0145f49ece42d4e60050aaa7594b5f2f38a33dd7251f529a2e6bad0704a51d
-size 6126
+oid sha256:2058d55314b63eb1c7c0b04c34acb1e3b9600d5392dfbaf477422b941c53ccbf
+size 6553
