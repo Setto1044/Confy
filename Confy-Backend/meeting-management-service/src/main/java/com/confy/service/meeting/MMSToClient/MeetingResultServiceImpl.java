b8e035a16791318e0ea89635f0bb60038dcc7520
배석진 <bsj1044@naver.com>
2025-02-11 19:28:23 +0900

    🚀 Chore: 배포 세팅
    Former-commit-id: 5767e50a5bed58510d5de6ed6870b283f155d3ab

--- Confy-Backend/meeting-management-service/src/main/java/com/confy/service/meeting/MMSToClient/MeetingResultServiceImpl.java
+++ Confy-Backend/meeting-management-service/src/main/java/com/confy/service/meeting/MMSToClient/MeetingResultServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c67de2dc845f65800f8a79d5effe5225045dee73df3eda3d171d9dd136dd4312
-size 6416
+oid sha256:a97c45902a2b656c330d8ceb751f3d0ec2ce3f51cc47e0d195074a548503e14b
+size 6500
