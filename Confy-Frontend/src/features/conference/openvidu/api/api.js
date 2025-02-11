a3088d9e721c094e5d0a0e6446ec88f69a247fb9
a99yyoung <a99yyoung@naver.com>
2025-02-11 19:30:36 +0900

    ✨ Feat : Openvidu 동의서
    Former-commit-id: 447204dd0f47a2aaa8aafba3b3ad921aad7db515

--- Confy-Frontend/src/features/conference/openvidu/api/api.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7de0b81d4dad498bc95766f3718aefc0ee801a248c51499fdcc160c9d661d044
-size 2028
--- Confy-Frontend/src/shared/api/apiClient/authClient.js
+++ Confy-Frontend/src/shared/api/apiClient/authClient.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1d1d7e226085ef87c0c6494a6386e6ce462bc62136ac79e9b8a5163dc67b596
-size 552
+oid sha256:3b81d3f801880852b71e57dfa85f338b556a60cbd76852c4b0307c6d23f4d927
+size 869
