e43c6bebecbb8e76186f2e5de05a7a5e2de11154
KMJ <kmjk1117@gmail.com>
2025-02-12 11:04:24 +0900

    💄 Design : 화상회의 중 페이지- 버튼 위치 수정
    버튼 위치 아래로 옮김, 버튼 텍스트 한글로 바꿈
    
    
    Former-commit-id: e94d7a7783029f99c1d922b36c859cfd2e5b649c

--- Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
+++ Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f6eb3bd6d3810b1f11a0f83406167b1e523c3d86d09ca361e1dd375d1294dc7
-size 22147
+oid sha256:2feaddd8803e64fea3131808eb1f0cbf8a4b15ae608bbd9caa352083ab75275d
+size 22203
