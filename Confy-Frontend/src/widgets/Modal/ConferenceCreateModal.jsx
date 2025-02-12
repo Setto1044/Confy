4f21edae574410d2c627a12a7038af8d07485e0f
a99yyoung <a99yyoung@naver.com>
2025-02-12 09:38:32 +0900

    ✏️ Fix : env 파일 수정
    Former-commit-id: 1d5fda823dc4e0b3c4dfc8af8e886d381f45a6f3

--- Confy-Frontend/src/widgets/Modal/ConferenceCreateModal.jsx
+++ Confy-Frontend/src/widgets/Modal/ConferenceCreateModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8230d1bfe3478ca675e932a3183f4a89a2cf8f34c26c90dcafccb35a66f24025
-size 5525
+oid sha256:5b66f65f38603429cb774d66622714a76ea191425890bb5caac96fb8ce1c8b22
+size 5531
