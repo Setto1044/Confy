875da5df20e99e30cea8d875528915ce27062c91
a99yyoung <a99yyoung@naver.com>
2025-02-12 10:35:14 +0900

    ✏️ Fix : env 파일 수정
    Former-commit-id: ae379da6e6104294f3676dfe2b50c9d938cfb021

--- Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
+++ Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf4bb5e62c7bd1cdd4e1dd4e6c5024fbebb710ab450be3624eb943919ace96da
-size 22197
+oid sha256:2f6eb3bd6d3810b1f11a0f83406167b1e523c3d86d09ca361e1dd375d1294dc7
+size 22147
