48af1d941ef23632716fa81dd0d48ee54796cfb0
KMJ <kmjk1117@gmail.com>
2025-02-12 15:14:50 +0900

    💄 Design : 사이드바 구현
    Former-commit-id: 77b13967f9d3051702db766a9ec542a16285a7af

--- Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
+++ Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a14ddca56b89f37a82c843c2f5589539d54e4b043fae5c1a25a5cc1019e1780a
-size 22392
+oid sha256:d20aec9948ae8e042efc2d86f1ffa1fae864ef7a0ce9f435807f912597ef0bf7
+size 22560
