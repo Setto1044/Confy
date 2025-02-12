9a3d197cd7487af073aa96801578f4b04e122ce8
a99yyoung <a99yyoung@naver.com>
2025-02-12 13:04:31 +0900

    💄 Design : 프로필 모달과 회원 정보 연결
    Former-commit-id: c07d438244f810a3922c480dc0aed86ea71d7301

--- Confy-Frontend/src/widgets/Header/MainHeader.jsx
+++ Confy-Frontend/src/widgets/Header/MainHeader.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:063238679232358dc0511c8dacce717e1882351e20f0732e22d0ef0a6c2af0a6
-size 2836
+oid sha256:b31f24fe9244092b85044cc08e53c51c9404cece730fc771ba904042b6ccef7b
+size 2810
