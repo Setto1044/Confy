53d4cbf37748b9a60874e2b82b8ae6347b976221
KMJ <kmjk1117@gmail.com>
2025-02-13 11:25:00 +0900

    ✨ Feat : 오픈비두 컨테이너에 참여자수 기능 redux에서 불러오기
    Former-commit-id: fcefdf80f5e016bc8e4bc7a3da70b0d80e5461bc

--- Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
+++ Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ee7939581f3fa2749567592da4c6e316c3118a09bf54eedd51a63a2a0e96087
-size 20888
+oid sha256:f3cbcd9ab869a5f4e56cd458ec8c059e7e267a9e99799c379d3049b2487b2150
+size 20723
