71348970c9d6329f9b446409699e75b7d701eba0
a99yyoung <a99yyoung@naver.com>
2025-02-13 10:23:24 +0900

    ✨ Feat : 로고 수정
    Former-commit-id: 27e4a1d8cfeaa39c689f995ff09a3d21f9deb880

--- Confy-Frontend/src/widgets/Modal/ProfileModal.jsx
+++ Confy-Frontend/src/widgets/Modal/ProfileModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b39d93da2495a132a6ddac713ea8ccfdc32d674710968073db03600c2d6d9c41
-size 2496
+oid sha256:6cf78ade7def9d76f492be0eb81617fd0980cbe53bf46b4c1f3299a51e2bd48f
+size 7172
