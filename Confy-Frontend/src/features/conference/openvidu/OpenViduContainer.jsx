5e06a8697e3de9d03333c19c7de6847becb35b14
KMJ <kmjk1117@gmail.com>
2025-02-13 17:10:55 +0900

    ✨ Feat : 회의 나가기, 회의 종료 axios 완료
    Former-commit-id: f998ea8b5710dff529079aabe273c3902450cb35

--- Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
+++ Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9dbec96ed89c45844d8e173e6281cfd899735d888167990070eae26ffdf347ae
-size 21233
+oid sha256:cf2dce8e31c0967756c3c2308188e677ff6bb042de237e416cdccbf4c9071d94
+size 23420
