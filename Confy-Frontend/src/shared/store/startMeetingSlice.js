4d129ac21be84fdd34de0e11dba4442c85a17eea
a99yyoung <a99yyoung@naver.com>
2025-02-13 17:20:53 +0900

    ✨ Feat : 회의 요약 기능
    Former-commit-id: 2e5e4c26c855d23d6c961fdb50fa0e63fa46db4e

--- Confy-Frontend/src/shared/store/startMeetingSlice.js
+++ Confy-Frontend/src/shared/store/startMeetingSlice.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a73ffaeb226eed96534ced0d06493fb5fd995e2a89a71934646e077f5dbd855
-size 754
+oid sha256:16931c3d8672057462da167b24cb828775cc8b5ca0f8c5d4e145e3d8c233c650
+size 1041
