07baeeb9374a29484ea8752cc644c037eeb3673a
a99yyoung <a99yyoung@naver.com>
2025-02-13 18:07:48 +0900

    ✨ Feat : Remove Name
    Former-commit-id: 4161e8f0ecafe73a4be36304c969f844dbffef51

--- Confy-Frontend/src/widgets/Modal/ConferenceReserveModal.jsx
+++ Confy-Frontend/src/widgets/Modal/ConferenceReserveModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad7e935bd69be895d534f5137532e6be9f44e1b18262c2763c290dec8f31faf6
-size 4896
+oid sha256:a7d8520cd25b9b684b9df26612d250387820cb05906eb3152787c31386bf11f4
+size 4819
