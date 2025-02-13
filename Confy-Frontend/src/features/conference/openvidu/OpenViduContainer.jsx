e8f9eef595767da66e62459c4422cb3e8060be30
a99yyoung <a99yyoung@naver.com>
2025-02-13 17:20:53 +0900

    ✨ Feat : 회의 요약 기능
    Former-commit-id: 2e5e4c26c855d23d6c961fdb50fa0e63fa46db4e

--- Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
+++ Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9dbec96ed89c45844d8e173e6281cfd899735d888167990070eae26ffdf347ae
-size 21233
+oid sha256:6f89c6b6e64df89d841b26a0089b2b64c401aa52c45e3ace61ee483df5b934de
+size 21592
