4b5a6bc5df4da2e59786e79a3669c10139005716
a99yyoung <a99yyoung@naver.com>
2025-02-15 00:51:24 +0900

    💄 Design : openvidu 화면 grid로 변경
    Former-commit-id: 9120968dd4a9e27d433b1ac997f65f423c0221c2

--- Confy-Frontend/src/features/conference/Sidebar/SummaryRequest/SummaryRequest.jsx
+++ Confy-Frontend/src/features/conference/Sidebar/SummaryRequest/SummaryRequest.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da38e707e93bbc812d53d2cb63aa9a1a1e1982ce40017a153c5653a2036e2514
-size 4637
+oid sha256:afa34b2992cb0592d2f0a55a8ba5d6ad844124821494105b3d0eb43b856fb7b8
+size 4737
