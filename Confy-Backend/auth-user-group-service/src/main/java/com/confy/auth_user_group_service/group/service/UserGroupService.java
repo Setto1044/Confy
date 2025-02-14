5e6fbe1d9d9e2495956ecf63bd9388c599beaeaa
배석진 <bsj1044@naver.com>
2025-02-14 16:52:46 +0900

    set new branch for leave group
    Former-commit-id: 7cafec4948a761ee5e4c21488a176a35f9c6d160

--- Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/group/service/UserGroupService.java
+++ Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/group/service/UserGroupService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2eda107e4314f84a64baeeb3ab67f4a65e42d7f12b2f6f280c31c72eb07fb7a6
-size 983
+oid sha256:43a19efeee8ef5db45b119327c4a71e3e1f83b8d2556cf5bda6eb6c506cddd1b
+size 1075
