430e7cb6853a93ba89bac07524297aa0442640a5
배석진 <bsj1044@naver.com>
2025-02-14 17:01:10 +0900

    ♻️ Refactor: 테스트용 로그 추가 및 로그 포맷 수정
    Former-commit-id: 936ccc868b5ef69c528c4948458d57cfe63077bc

--- Confy-Backend/api-gateway-service/src/main/java/com/confy/api_gateway_service/common/filter/AuthenticationFilter.java
+++ Confy-Backend/api-gateway-service/src/main/java/com/confy/api_gateway_service/common/filter/AuthenticationFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:874cf037d8070e300a12ed94e06a2486c76f5a1446f8afc04f35b1f51c3a92da
-size 6379
+oid sha256:2e1e8c69cfffcf2bb4fbee58d5b3f3d3dbf9b0920c978e814e46f20f4e6f94e3
+size 6279
