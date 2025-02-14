7f35c3d06501bec080c7553fbbbcd0e63eb6f185
배석진 <bsj1044@naver.com>
2025-02-14 13:54:03 +0900

    ♻️ Refactor: 회원 그룹 나가기 기능 추가 및 그룹 관련 오류 메시지 상세화
    Former-commit-id: 8e7e12b6e5549026edf88c1ee4abc156a8009891

--- Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/group/controller/GroupController.java
+++ Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/group/controller/GroupController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7abf18a52c1707c32c1e914d0075f953e7e730be0395a8f2589ee0573ce64282
-size 4148
+oid sha256:7f4a25684910bcacf1de05424d5e4434c6e0b95422bd7bdb0d9b683f6916ab14
+size 4149
