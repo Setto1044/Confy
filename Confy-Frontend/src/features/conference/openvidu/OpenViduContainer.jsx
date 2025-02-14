f4a1b570064090ec67c3c4beeecb64b915d11e67
KMJ <kmjk1117@gmail.com>
2025-02-14 10:15:53 +0900

    ✨ Feat : 참여자 이름 input 기능 구현
    Former-commit-id: 11ef92ae6798cf5df9757dc9b3b4f657acebc49f

--- Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
+++ Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62877eaca725bea42f2e71b55d42f994f3664e45d1429289cebdefa9f6c78dbd
-size 23764
+oid sha256:a60a64a4aa04774d0cb52dbe5e1da1c45d1169820ac78ddaecb5ba646a5f829e
+size 24714
