7dc36dd72729ad7ac25477bd90f57d0a6a62f6d9
배석진 <bsj1044@naver.com>
2025-02-14 13:56:15 +0900

    ♻️ Refactor: 회원 초대 메서드 불필요한 로그 삭제
    Former-commit-id: c8a1c5f724624a27a22e9b3f1545d36cc1af6ed5

--- Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/group/service/UserGroupServiceImpl.java
+++ Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/group/service/UserGroupServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23ae74139ca93f78643843727fae90f4e44302e3fbbc173c255ec86fd5dc38b4
-size 5714
+oid sha256:1a517fa6213899c2b4ea553af857d6a0867c3a81a77f9eb38e97ea01e0dccf69
+size 5616
