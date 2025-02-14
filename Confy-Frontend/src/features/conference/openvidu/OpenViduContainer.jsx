c65f35f356f97d8b312a66c68c4128512bb06510
KMJ <kmjk1117@gmail.com>
2025-02-14 10:22:33 +0900

    💄 Design : 회의 참여 화면 css 수정
    Former-commit-id: a88a1083d11ceeab515335bc0c1ee561d3a8b488

--- Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
+++ Confy-Frontend/src/features/conference/openvidu/OpenViduContainer.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a60a64a4aa04774d0cb52dbe5e1da1c45d1169820ac78ddaecb5ba646a5f829e
+oid sha256:6fd25efdde9c7fa9eed47fd0dfc50b679db413819e773452570285e2a0878368
 size 24714
