0ed74d45cf972c01e8987ee880588ac78867fcf1
KMJ <kmjk1117@gmail.com>
2025-02-16 00:26:50 +0900

    ✨ Feat : 요약정리 스크롤 생성
    Former-commit-id: 3557966c2ee4e235733f50abd27d317c8bb0ae6d

--- Confy-Frontend/src/pages/EditVisualizationPage/EditVisualizationPage.jsx
+++ Confy-Frontend/src/pages/EditVisualizationPage/EditVisualizationPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a123023434df35e36b367e4acea20f1e57c03ac5214760ea58321c02a68892d0
-size 3195
+oid sha256:76294e073d2e9b518075c4420efc2d0a3339a6b20dbacf4fb26a936b572c00e6
+size 3444
--- Confy-Frontend/src/pages/SummaryPage/SummaryPage.jsx
+++ Confy-Frontend/src/pages/SummaryPage/SummaryPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:660703934a40efb268e805fd6be4720d1ba829895b0453ffa3f550ea82422216
-size 4584
+oid sha256:d43a97226c4692ec558114c2fc69f71e013bda2b846bceddce05168162a510fc
+size 5018
