02e462d0af25f2d99714a75b7c40f822f20c8f40
KMJ <kmjk1117@gmail.com>
2025-02-16 01:21:29 +0900

    💄 Design : 요약페이지 스크립트 스크롤 수정
    스크롤 끝까지 해도 내용 보이도록 하단 여백 줌
    
    
    Former-commit-id: 2b21d74edfd56f89cc9d038092219433cec9d73d

--- Confy-Frontend/src/features/summary/ui/Panels/SummaryPanel/SummaryPanel.jsx
+++ Confy-Frontend/src/features/summary/ui/Panels/SummaryPanel/SummaryPanel.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0c9490fd3a5313fec22cd70afdde381bb6245c5c0d96981367de23d7f0136f3
-size 1413
+oid sha256:63fc2bbb21d21363fbc4d60c3aae46d10d2954ce8192c621cc9853a625338545
+size 2253
