a9d2bb7e03c19e673b8615b1fb99500d84eceff9
KMJ <kmjk1117@gmail.com>
2025-02-16 02:33:35 +0900

    ✨ Feat : 요약페이지의 헤더에 main페이지 네비게이터 구현
    Former-commit-id: fb271fbc2ffdbdd4f8c454c2d5a608a2aee962f0

--- Confy-Frontend/src/features/summary/ui/SummaryInfoHeader/SummaryInfoHeader.jsx
+++ Confy-Frontend/src/features/summary/ui/SummaryInfoHeader/SummaryInfoHeader.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a716728b3b602432d1f662bf8f48dd9195ce8ec3880e8b6057e2a076ed104b6
-size 1098
+oid sha256:2ba8fbc7ee71707f84c96ef85738ccf8ed19e4d9dbedd61da5ad1935195850cd
+size 1385
