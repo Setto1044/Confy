d63e0c60417412e4d69c1f08d6e3826d001c2417
a99yyoung <a99yyoung@naver.com>
2025-02-15 16:18:24 +0900

    🐛 Fix : OpenVidu 요약 안뜨는 문제 해결
    Former-commit-id: c7666f6ae67397214c5489efa0b5d6d6c7cb3826

--- Confy-Frontend/src/features/summary/api/summaryApi.js
+++ Confy-Frontend/src/features/summary/api/summaryApi.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f40bef31a4c00412ab6c5a0efad5d2752383dc0de2cb81f6cac173fb5371c4ce
-size 1518
+oid sha256:d2f33c8cfb2c2827f8772eaa21afd4c2ae540cb597a9bb26ed847c053a37de3c
+size 1519
