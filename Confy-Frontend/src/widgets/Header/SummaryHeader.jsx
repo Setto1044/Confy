c6d7c80518696a14902564e7d32f6ad19b0eba45
KMJ <kmjk1117@gmail.com>
2025-02-16 00:43:14 +0900

    💄 Design : 요약페이지, 시각화 수정 페이지 헤더 수정
    Former-commit-id: 413ea7aa75afb4e05747190d933be9b6fb2d0565

--- Confy-Frontend/src/widgets/Header/SummaryHeader.jsx
+++ Confy-Frontend/src/widgets/Header/SummaryHeader.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:879de8fae777050d22404be1c11348d4186af9a2a39fd2e948cc7828b174fd16
-size 3070
+oid sha256:9ea97ee947c79885e57ed40ecbffe1946dcd3fb048e2b61f59ba748e3d836d9d
+size 3023
