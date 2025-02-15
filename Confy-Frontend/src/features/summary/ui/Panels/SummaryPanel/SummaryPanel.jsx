90ee1d2141d946a2ff0b5b8db9fb8dd642660ab7
KMJ <kmjk1117@gmail.com>
2025-02-16 00:56:16 +0900

    💄 Design : 요약페이지 요약정리, 시각화 버튼 위치 수정
    Former-commit-id: b4bf2db2511fe96a50114fca44e3a1c88a53d6a6

--- Confy-Frontend/src/features/summary/ui/Panels/SummaryPanel/SummaryPanel.jsx
+++ Confy-Frontend/src/features/summary/ui/Panels/SummaryPanel/SummaryPanel.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c08a9a7e9c8bfb9f85f1f8b6112c99b5e0e52ffd6920dfd1ddac203e3535f824
-size 1322
+oid sha256:d0c9490fd3a5313fec22cd70afdde381bb6245c5c0d96981367de23d7f0136f3
+size 1413
