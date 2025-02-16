255909cc57f58dadd591a0061a709c4f1119ccf2
KMJ <kmjk1117@gmail.com>
2025-02-16 17:50:47 +0900

    ✨ Feat : 시각화 수정 페이지에 시각화 데이터 불러오기
    Former-commit-id: 1d57c2b6268de2f13556024796fe6ea0d47e89a4

--- Confy-Frontend/src/features/summary/ui/Panels/VisualizationPanel/VisualizationPanel.jsx
+++ Confy-Frontend/src/features/summary/ui/Panels/VisualizationPanel/VisualizationPanel.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98e8ea0a55a507ef897273f923f51492d70428acd53bb6aab3a8bdb320c16113
-size 4359
+oid sha256:61bc888c9a37a20d7224e679376474057fcbb2639b1d67032a20ad432611ef0a
+size 4496
