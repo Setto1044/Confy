393a259565e906e18d9cccba286735a2fe5877cf
KMJ <kmjk1117@gmail.com>
2025-02-16 19:06:34 +0900

    ✅ Check : 시각화 수정 모드 전환, 시각화 수정 데이터 저장 확인
    Former-commit-id: 5454f3988a1d917fda0104c0f501ba8be481e688

--- Confy-Frontend/src/features/edit-visualization/api/editVisualizationApi.js
+++ Confy-Frontend/src/features/edit-visualization/api/editVisualizationApi.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b1cede2b5122e03cfb83a17784c8fd4fe4e7d578bc832a0cc2e5b2e971cd913
-size 1396
+oid sha256:19e0a0d099c2027da2a5fad1bb03ec67669dfb174a24bfcd990f123d5d7d221e
+size 1406
