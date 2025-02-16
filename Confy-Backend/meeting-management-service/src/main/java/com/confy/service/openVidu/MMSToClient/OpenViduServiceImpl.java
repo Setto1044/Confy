c156a72f8940f84fda81be4622a01ccca5092eaa
노영단 <shdbwjd7052@naver.com>
2025-02-16 13:29:23 +0900

    ✨ Feat: Add API to fetch meeting participant names
    특정 회의에 참가한 참가자들의 이름을 조회하는 API 추가
    
    
    Former-commit-id: e56e4e9fc1878c38e40614c40f3b13810cdb945d

--- Confy-Backend/meeting-management-service/src/main/java/com/confy/service/openVidu/MMSToClient/OpenViduServiceImpl.java
+++ Confy-Backend/meeting-management-service/src/main/java/com/confy/service/openVidu/MMSToClient/OpenViduServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d9c770b4bf2feb5c6fd51cc8cacdd1da294cd9f3de54257ae3dd0811c7f5914
-size 3924
+oid sha256:0083df12ff0e9862e15d626ab05a55138218bad963fc906df62b4941ca7be2d3
+size 4051
