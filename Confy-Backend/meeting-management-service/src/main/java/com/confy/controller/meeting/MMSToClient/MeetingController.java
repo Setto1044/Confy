c41a02fa1b3c152558430fd5e010c97968949e3f
노영단 <shdbwjd7052@naver.com>
2025-02-16 17:21:30 +0900

    ✨ Feat: Send the number of remaining participants when leaving a meeting
    회의 참가자가 회의 방을 나갈 때, 남아있는 참가자 수를 반환하도록 API 수정
    
    
    Former-commit-id: 29cbf746951520f154bddb245ad1f4f170a5b024

--- Confy-Backend/meeting-management-service/src/main/java/com/confy/controller/meeting/MMSToClient/MeetingController.java
+++ Confy-Backend/meeting-management-service/src/main/java/com/confy/controller/meeting/MMSToClient/MeetingController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:828e30bc1a14c1e5ed566312918e745ec3308389dc3536d72fa5fa5410eddbe6
-size 7750
+oid sha256:e64a90b605ba8f513861534ff99d07423cf3a1298dc7e1623aa064e84570b053
+size 8146
