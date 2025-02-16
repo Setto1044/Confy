47185242a476b27ae5fd401ff82be62910d2fe3a
yejin <kimyejin0044@gmail.com>
2025-02-16 19:32:25 +0900

    :speech_balloon: Refactor: GPT 프롬프트 개선, Tree 함수 모듈화
    Former-commit-id: 6bdb7165c78e843cbef4afeda42af8f901b4af29

--- Confy-Backend/gpt-gateway-service/src/main/java/com/confy/gpt_gateway_service/client/OpenAIClient.java
+++ Confy-Backend/gpt-gateway-service/src/main/java/com/confy/gpt_gateway_service/client/OpenAIClient.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffab45371888398a41855ea202e4cfc90ba09fca0b973f34ec4386d8199dffc0
-size 5964
+oid sha256:2d4c51f08e3b6cb01321c9220899bc5050b1f47f2c2f8d110e5e7053578f951b
+size 6071
