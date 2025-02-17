01c9f2a4ab47be802615c485d8c4009e95268181
yejin <kimyejin0044@gmail.com>
2025-02-17 17:40:03 +0900

    :speech_balloon: Refactor: GPT 프롬프트 개선
    Former-commit-id: b948e303caba2223359278d067d92d0628af4bc4

--- Confy-Backend/gpt-gateway-service/src/main/java/com/confy/gpt_gateway_service/service/GptService.java
+++ Confy-Backend/gpt-gateway-service/src/main/java/com/confy/gpt_gateway_service/service/GptService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e09d175839e3c32e93ff7b8eed32db04a21c410da0b7886fb26fd84f8562790
-size 7693
+oid sha256:cb6db3dad99fc17d8ff90189ee970f88c35e6bc5df2d2533e7245f9020a5294a
+size 6878
