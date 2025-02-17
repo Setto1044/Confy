48165cc4d744dd9344e3c21b7c2b522515b1b7b8
a99yyoung <a99yyoung@naver.com>
2025-02-17 12:28:02 +0900

    🐛 Fix : 그룹원 스크롤, 그룹 스크롤 추가
    Former-commit-id: 776294bd12bff939c19fefaaca33b1e6c79bd9c0

--- Confy-Frontend/src/features/summary/ui/SummaryInfoHeader/SummaryInfoHeader.jsx
+++ Confy-Frontend/src/features/summary/ui/SummaryInfoHeader/SummaryInfoHeader.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d764267e2daabe9cb23dc9dafcffd6f4f09c97db5c171eb612a761b2f2a0921c
-size 1759
+oid sha256:554fc02ebb7287a907f29c2a2bb35d2ddfc362b9b00719ba823cb3d310260f5f
+size 1751
