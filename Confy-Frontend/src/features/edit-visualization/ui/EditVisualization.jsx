45e8ea40963e145dfb0df72ddbe0d2436d708732
배석진 <bsj1044@naver.com>
2025-02-18 00:43:06 +0900

    🚧 Chore: 시각화 저장, 엣지 누락 수정 필요요
    Former-commit-id: 10a219614d9fc01a7215d10019f1879aea4ae2f7

--- Confy-Frontend/src/features/edit-visualization/ui/EditVisualization.jsx
+++ Confy-Frontend/src/features/edit-visualization/ui/EditVisualization.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8d5879b1f0aa131ba72b38bbd2fe4338a85d73ddd04b63d6ceab43c644816f2
-size 2789
+oid sha256:934f84a420a40ae4b608521e81debc6d1b4316a5b66923442b4b0e1f37e5ebcc
+size 4629
