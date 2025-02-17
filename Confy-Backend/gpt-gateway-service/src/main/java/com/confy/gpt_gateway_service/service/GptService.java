897dda9a07acddfe3e49fdd3eff3fab77e961ec9
yejin <kimyejin0044@gmail.com>
2025-02-17 13:00:51 +0900

    :goal_net: Fix: 스크립트 DB 저장 이후 GPT 요약 요청 동기 실행되도록 수정
    Former-commit-id: 80d18bc827a09ce0eed793b4b2e47cc3590332d0

--- Confy-Backend/gpt-gateway-service/src/main/java/com/confy/gpt_gateway_service/service/GptService.java
+++ Confy-Backend/gpt-gateway-service/src/main/java/com/confy/gpt_gateway_service/service/GptService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c1e3c8cf42eeba193abc643abba6c1aefa568911cfc1266db1677b7b3ac28c0
-size 5975
+oid sha256:ee7f63a5d4648aaef2dc2b82a46054884b29785de4cc580784a9f29664e3b148
+size 6029
