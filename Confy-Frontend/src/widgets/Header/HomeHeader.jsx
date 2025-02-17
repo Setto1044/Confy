a2ff53f99245cd97f974e82174d98659fa8e4bdc
kmj <kmjk1117@gmail.com>
2025-02-17 23:59:11 +0900

    🎨 Improve : 홈 화면 수정
    Former-commit-id: 192c78368bae0d97bddb9ae24e8fa9d6eebb0bc2

--- Confy-Frontend/src/widgets/Header/HomeHeader.jsx
+++ Confy-Frontend/src/widgets/Header/HomeHeader.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d786eef5848271eb39d05dfa938568d23297a9877ff57876b56fd0aef9b8f29b
-size 1625
+oid sha256:711c3367aae07eb0f8105a8a761ca112e1faa2b911fabb05d4e66682895b1b63
+size 1624
