da736f23227b2b2bfd3965b11c07e619464a1835
a99yyoung <a99yyoung@naver.com>
2025-02-17 16:15:59 +0900

    🐛 Fix : 회의 중 스크립트 자동 스크롤
    Former-commit-id: 17c2e7c8aeb7bc3a20e2d57a9caab69bcbd71ebf

--- Confy-Frontend/src/features/edit-visualization/ui/ReactFlow/NodeEditInput.jsx
+++ Confy-Frontend/src/features/edit-visualization/ui/ReactFlow/NodeEditInput.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3dbde1ebfb1bcf98ab538b9727c32e17088d50e46d16acaf6309255f3a2d6259
-size 892
+oid sha256:141cfbd41d7b345e5efc6c7b965ce44c4b3f0bbf6251e16890184963f52f98d3
+size 895
