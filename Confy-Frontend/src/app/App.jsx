6047b1f2286ebccad71414934dabaccb83f26c68
KMJ <kmjk1117@gmail.com>
2025-02-17 09:49:16 +0900

    🐛 Fix : 메타 태그 오류 수정
    App.jsx에 HelmetProvider 추가
    
    
    Former-commit-id: ee35ef4092e0ab7906285aded15cedefb30220ed

--- Confy-Frontend/src/app/App.jsx
+++ Confy-Frontend/src/app/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e2e4b640ad3560f37581b73b7cf1f42cb3a4b146f4e5ab811d59752a1eacf98
-size 653
+oid sha256:a5dba3437c1dbf79eabcd88cf9b373fbc36b2a319e0d07e3e53709116013e6d8
+size 778
