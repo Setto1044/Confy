81cf079fc1002a80515687d58b2d1e6f1fdf0cc7
예세림 <yesellya@gmail.com>
2025-02-17 20:59:59 +0900

    🚧 Feat: 실시간 알림 기능(SSE) 구독 자동화 및 그룹 초대 알림 구현
    Former-commit-id: e6f8714d01957b0499d161666ac97a561e59f279

--- Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/notification/controller/NotificationController.java
+++ Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/notification/controller/NotificationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65ef72eb225467cd89893d131733e9d577df05e3608d2eeea23e63e47288e468
-size 2152
+oid sha256:7eba0e630ea4c46b63dbc6a619c31c3c961d257dbf54c28a7074bc4f2aeaf8a5
+size 2138
