58804c83953c673362ec3812d38b1afdeb3349a3
예세림 <yesellya@gmail.com>
2025-02-17 14:23:09 +0900

    ✨ Feat: 실시간 알림 기능(SSE) 구현-사용자화
    Former-commit-id: d937ccb43fe81fcf318dad2731081bd229e53385

--- Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/notification/service/NotificationService.java
+++ Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/notification/service/NotificationService.java
@@ -1,5 +1,3 @@
-package com.confy.auth_user_group_service.notification.service;
-
-public interface NotificationService {
-    void checkMeetingsForNotifications();
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:bbe89e320e96023b77f70124714664ab8b395cec01f731598da3e23a9393d25b
+size 546
