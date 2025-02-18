7a38e33380b2feaf7f1479205c0f4a9af1bfac47
a99yyoung <a99yyoung@naver.com>
2025-02-18 12:05:09 +0900

    ✨ Feat : 요약 키워드 api 연결
    Former-commit-id: eeb047ad85c8529f5988a667f00b822c02b66b20

--- Confy-Frontend/src/widgets/Header/SearchBar.jsx
+++ Confy-Frontend/src/widgets/Header/SearchBar.jsx
@@ -1,16 +1,3 @@
-import React, { useState } from "react";
-import icon from "../../assets/icons/magnifying-glass-solid.svg";
-import styles from "../Header/SearchBar.module.css";
-
-const SearchBar = ({ onSearch }) => {
-  return (
-    <div className={styles.searchBar}>
-      <button>
-        <img src={icon} alt="" />
-      </button>
-      <input type="text" className={styles.inputField} />
-    </div>
-  );
-};
-
-export default SearchBar;
+version https://git-lfs.github.com/spec/v1
+oid sha256:61910e0f36352cacea7709c77cdf22c4972c6f45cb7dcf292c49fe14f299fbcb
+size 1598
