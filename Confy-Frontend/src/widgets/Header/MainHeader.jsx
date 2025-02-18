4cdbfbcfb2dd4ffde752b2d3eab8b14af6f2d90e
a99yyoung <a99yyoung@naver.com>
2025-02-18 12:05:09 +0900

    ✨ Feat : 요약 키워드 api 연결
    Former-commit-id: eeb047ad85c8529f5988a667f00b822c02b66b20

--- Confy-Frontend/src/widgets/Header/MainHeader.jsx
+++ Confy-Frontend/src/widgets/Header/MainHeader.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0abb153b39bf95f60e84e89125b9a0aba9b326743a848497c7f702de7dec3667
-size 3235
+oid sha256:8014daaabefa304b4f7121a76275636974e1b3bb7b164defb9e58e840313136c
+size 3404
