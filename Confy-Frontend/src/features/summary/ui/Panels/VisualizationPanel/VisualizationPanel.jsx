9129021cca3852d78d24bba781673fb4f08e75a6
배석진 <bsj1044@naver.com>
2025-02-18 17:21:40 +0900

    🐛️ Fix: 이미지 저장 기능 추가 및 그룹 찾기 로그 수정
    Former-commit-id: 37d838ceb52f887417c6035bc3c319b2c3fce6e0

--- Confy-Frontend/src/features/summary/ui/Panels/VisualizationPanel/VisualizationPanel.jsx
+++ Confy-Frontend/src/features/summary/ui/Panels/VisualizationPanel/VisualizationPanel.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89d936e7c790a128555b3228fe1e38c1e9681227e029ede0a6fdea77b8896dc9
-size 3301
+oid sha256:e46f58139e279a2fbd4f6c8fe1110f9f9caead41d2d62af931a2a38367a94840
+size 3408
