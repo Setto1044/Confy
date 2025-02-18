effae3f47e189da75c4affec75b305bbfe3a923d
배석진 <bsj1044@naver.com>
2025-02-18 18:03:18 +0900

    🐛️ Fix: 그룹에 회원 추가 로그 검증 추가
    Former-commit-id: 87457e5736f1b3bd13af961f6f8546dbf85f8853

--- Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/group/service/UserGroupServiceImpl.java
+++ Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/group/service/UserGroupServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab762c7ccb629cd5f86be712b6aaede6f9ccc9659eff44356df54a188d223e05
-size 7776
+oid sha256:a2dcfb820042e57f936afdf26b79f1637b377c23c6849ee7f137f42965ab7750
+size 7943
