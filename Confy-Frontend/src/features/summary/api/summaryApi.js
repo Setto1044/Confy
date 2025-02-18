1e54b665100ffc305de1b87c5a9980f5a188ff47
KMJ <kmjk1117@gmail.com>
2025-02-18 15:02:11 +0900

    ✨ Feat : 요약페이지에 새로운 시각화 응답데이터 띄움
    Former-commit-id: 9ed556fad8fce79dec8e1c1ac5277264f0e89a9e

--- Confy-Frontend/src/features/summary/api/summaryApi.js
+++ Confy-Frontend/src/features/summary/api/summaryApi.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad724c0875f52f954285552d562bee90dd4b56c2764371607989ddaeb4f26be7
-size 4053
+oid sha256:49c7fd1d9b89b170246faa6fc13c5ae3a62fc5d9582677f401ca9e7a45fbad56
+size 5051
--- Confy-Frontend/src/features/summary/ui/Panels/VisualizationPanel/VisualizationPanel.jsx
+++ Confy-Frontend/src/features/summary/ui/Panels/VisualizationPanel/VisualizationPanel.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89d936e7c790a128555b3228fe1e38c1e9681227e029ede0a6fdea77b8896dc9
-size 3301
+oid sha256:17f66017df9d6b1d7dbf484e01c16ca9427b0513ee1e7aa072057020b61a142e
+size 4072
