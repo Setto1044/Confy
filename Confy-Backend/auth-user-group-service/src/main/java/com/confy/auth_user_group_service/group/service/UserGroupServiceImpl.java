506714c53368105b76b6b37e803ae05b19ac7810
배석진 <bsj1044@naver.com>
2025-02-18 10:21:53 +0900

    🐛️ Fix: 그룹 탈퇴 회원 조회되는 문제 및 중복 초대 메시지 예외 문제 수정
    Former-commit-id: 8810b3f081e59f76c461958c6fd2e99bebfd12db

--- Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/group/service/UserGroupServiceImpl.java
+++ Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/group/service/UserGroupServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6d08cf3fe0d4c047b4aecfb3266b3060e8322ea40f863b0c2589fa869bd494f
-size 5644
+oid sha256:6b89a9b74f42b7df5ff018b4c2827483e23584758d34834b9b551944300b8c07
+size 5739
