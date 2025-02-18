a314f4b13a044437761d8155002568e7af5f6a56
예세림 <yesellya@gmail.com>
2025-02-18 11:09:44 +0900

    🚧 Feat: 실시간 알림 기능(SSE) 그룹 탈퇴 시 특정 그룹 알림 구독 해제, 시작 전 알림 / 예정 미팅 조회 로직 분리
    Former-commit-id: 54e1b2da3cb18a0f83328e2d28e20e707e33a83e

--- Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/notification/controller/NotificationController.java
+++ Confy-Backend/auth-user-group-service/src/main/java/com/confy/auth_user_group_service/notification/controller/NotificationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7eba0e630ea4c46b63dbc6a619c31c3c961d257dbf54c28a7074bc4f2aeaf8a5
-size 2138
+oid sha256:299b1de4478d37a999f3556a897213296f9bff55ea551663583b01e98cec1495
+size 2790
