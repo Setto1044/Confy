6bf27315afb6d75a41e9ceee3e26e934670f5237
a99yyoung <a99yyoung@naver.com>
2025-02-19 11:03:16 +0900

    🐛 Fix : 오류 수정
    Former-commit-id: 403cb90c7010a51b0d39a66e879772b48d27290d

--- Confy-Frontend/src/shared/hooks/useNotifications.js
+++ Confy-Frontend/src/shared/hooks/useNotifications.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c59941414538fb8d8938cb2f24422ee51ea53d591bc3b6a702fb95450b29b3e4
-size 1381
+oid sha256:76902468a445449fcf6a573927eed8e00b21ed5b05cf5399e5a0cacafea17e04
+size 1481
