aecd6911ce334fcdb22e40c48b9595f3cf50f9ae
yejin <kimyejin0044@gmail.com>
2025-02-19 14:52:40 +0900

    💬 Refactor: 키워드 쉼표 파싱 GPT 프롬프트 개선
    Former-commit-id: eea6b5a75da1285ae2f3fb2c5213af1dc6c88587

--- Confy-Backend/gpt-gateway-service/src/main/java/com/confy/gpt_gateway_service/service/GptService.java
+++ Confy-Backend/gpt-gateway-service/src/main/java/com/confy/gpt_gateway_service/service/GptService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ab3d6625b6fb8fe12c3563fc18f3ae7be8306f10857d1c39636783f8bbdeffa
-size 6269
+oid sha256:381dc69e7b7195d8d1a9f1c925f4b2a0bf8dda2f10a03f4c5fbbdfc99049e73d
+size 6299
