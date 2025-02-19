e3292b9a1166ad58c1e98b68807fc2ca1dc5b926
a99yyoung <a99yyoung@naver.com>
2025-02-19 14:23:49 +0900

    🐛 Fix : 콘솔 로그 지우기
    Former-commit-id: 2d34f22dc868d37a43430897be90ee2382af900e

--- Confy-Frontend/src/widgets/Modal/NotificationModal.jsx
+++ Confy-Frontend/src/widgets/Modal/NotificationModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28bd7aebd13a18b6453adff9887b1aeee0e5aa0d7f8020e500fcc10bdc7895bc
-size 1765
+oid sha256:93a7b0e00d151d1407c2a9f2f5aaadf642703901b8281e49962c7b197d421d2e
+size 1686
