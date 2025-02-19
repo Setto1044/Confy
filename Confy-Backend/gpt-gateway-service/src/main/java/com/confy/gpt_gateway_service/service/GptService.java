b23bfe31dc5bd7b5f0e2bafcd81f212e7c3c7c06
yejin <kimyejin0044@gmail.com>
2025-02-19 15:22:18 +0900

    💬 Refactor: 키워드 파싱 문제 개선한 GPT 프롬프트 업데이트
    Former-commit-id: 0b51792458ecc9686d5f656e67ad62b5dddb86a2

--- Confy-Backend/gpt-gateway-service/src/main/java/com/confy/gpt_gateway_service/service/GptService.java
+++ Confy-Backend/gpt-gateway-service/src/main/java/com/confy/gpt_gateway_service/service/GptService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:381dc69e7b7195d8d1a9f1c925f4b2a0bf8dda2f10a03f4c5fbbdfc99049e73d
-size 6299
+oid sha256:a300ca953d9abe603fcdadc721a178d67b54b7620572236dbe874e114cfbeca1
+size 6419
