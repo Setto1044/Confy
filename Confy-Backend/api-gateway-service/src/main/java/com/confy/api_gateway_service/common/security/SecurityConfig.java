fbe5b0e0969c4cf0a3a48d59b3e50bed1d18042b
배석진 <bsj1044@naver.com>
2025-02-19 10:18:26 +0900

    🐛 Fix: 모든 요청 접근 허용
    Former-commit-id: 6dcd43762a69931c6107a0be0541d6d8326e3ef6

--- Confy-Backend/api-gateway-service/src/main/java/com/confy/api_gateway_service/common/security/SecurityConfig.java
+++ Confy-Backend/api-gateway-service/src/main/java/com/confy/api_gateway_service/common/security/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c6b8f7e9297988144329de796c4576fe9519f1c47c0b26f37b724aaabfe82f0
-size 1269
+oid sha256:52eb2219795179e00aaafc10afd8cfb520deb48641c99c6e084d984642c5f1a6
+size 1176
