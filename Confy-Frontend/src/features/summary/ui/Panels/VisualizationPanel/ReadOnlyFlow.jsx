e1f45b9fc4728aae3d0f5011cebae46f1ce08f56
a99yyoung <a99yyoung@naver.com>
2025-02-19 10:35:45 +0900

    🐛 Fix : 다운로드버튼 import 이슈 해결
    Former-commit-id: 481c4008b55b03df6a271b399263fcdb8463b13b

--- Confy-Frontend/src/features/summary/ui/Panels/VisualizationPanel/ReadOnlyFlow.jsx
+++ Confy-Frontend/src/features/summary/ui/Panels/VisualizationPanel/ReadOnlyFlow.jsx
@@ -1,20 +1,20 @@
-import ReactFlow, { Background } from 'reactflow';
-import 'reactflow/dist/style.css';
+import ReactFlow, { Background } from "reactflow";
+import "reactflow/dist/style.css";
 
 const ReadOnlyFlow = ({ nodes, edges }) => (
-    <div className="w-full h-full border rounded-lg">
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        fitView
-        attributionPosition="bottom-right"
-        nodesDraggable={false}
-        nodesConnectable={false}
-        elementsSelectable={false}
-      >
-        <Background />
-      </ReactFlow>
-    </div>
-  );
+  <div className="w-full h-full border rounded-lg">
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      fitView
+      attributionPosition="bottom-right"
+      nodesDraggable={false}
+      nodesConnectable={false}
+      elementsSelectable={false}
+    >
+      <Background />
+    </ReactFlow>
+  </div>
+);
 
 export default ReadOnlyFlow;
