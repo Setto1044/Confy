ed2a6977a24dacf1f44fe27d4e707a8db1980f83
a99yyoung <a99yyoung@naver.com>
2025-02-20 15:57:52 +0900

    🐛 Fix : 충돌 해결
    Former-commit-id: de67297336b4c543ec5276e75662c2ed66a8d727

--- Confy-Frontend/src/features/main/Home/HomeList.jsx
+++ Confy-Frontend/src/features/main/Home/HomeList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91e311206e68306b163cb5bc2f9bb892ec7a3fac9332a915cf03466819293d5c
-size 3094
+oid sha256:124b7c99bcca3f524e2f90ef53116b5eab02cfac281887a7aa65cc47f61f1573
+size 2480
