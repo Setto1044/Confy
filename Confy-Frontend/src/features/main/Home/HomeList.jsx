980553feb67ae4ef4c17149e8e65ad488fe6b15c
a99yyoung <a99yyoung@naver.com>
2025-02-20 16:38:47 +0900

    🐛 Fix : 홈화면 라우터 설정
    Former-commit-id: bfd6b0e78268a5a4317abcaab36a4e551e3cf13d

--- Confy-Frontend/src/features/main/Home/HomeList.jsx
+++ Confy-Frontend/src/features/main/Home/HomeList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:124b7c99bcca3f524e2f90ef53116b5eab02cfac281887a7aa65cc47f61f1573
-size 2480
+oid sha256:6c6000dd6a65f151550af13c13b5d25950e518bf467053a99eefe96d2cf6157e
+size 2575
