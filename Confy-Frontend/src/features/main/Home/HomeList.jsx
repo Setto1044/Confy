aa29fddf52d4fcdb7b85e6abd94897585bf81dad
KMJ <kmjk1117@gmail.com>
2025-02-20 15:44:51 +0900

    ✨ Feat : 스크롤바 생성
    Former-commit-id: aad9373436a0e03d75184d36e8a464e96edf3afd

--- Confy-Frontend/src/features/main/Home/HomeList.jsx
+++ Confy-Frontend/src/features/main/Home/HomeList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21689140a938f99bec367530be3ea6bfa02b45a3840877d77bd3a8e09ac7a760
-size 2539
+oid sha256:124b7c99bcca3f524e2f90ef53116b5eab02cfac281887a7aa65cc47f61f1573
+size 2480
