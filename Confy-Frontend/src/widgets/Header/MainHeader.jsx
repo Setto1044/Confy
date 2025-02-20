01af3923c0f639504ee63c47920d881968440cfd
a99yyoung <a99yyoung@naver.com>
2025-02-20 15:42:13 +0900

    🐛 Fix : 프로필 이미지 안뜨는 오류 해결
    Former-commit-id: cda9ca5eaa66eafeb3ef2ca8d7992317c1ea2b8d

--- Confy-Frontend/src/widgets/Header/MainHeader.jsx
+++ Confy-Frontend/src/widgets/Header/MainHeader.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b88164ad169008e7c6ecf5d13514c80e32216081ebb8f417399adad5d8b2c96
-size 3336
+oid sha256:1920180600dd05f2fc8a38de8ffe0ee69323527a3a33de66c2729db4a86fed40
+size 3447
